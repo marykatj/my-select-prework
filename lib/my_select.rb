79d1ff67e8b0f0601b8c57d684b914830c777cde
Mary-Kate James <marykatj@gmail.com>
2018-05-09 18:15:41 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  select = []
+  while i < collection.length
+      if yield(collection[i]) == true
+      select << collection[i]
+    i += 1
+  end
+  select
 end
